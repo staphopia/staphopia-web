45a8ff48475d3f309c93186c38ccfb638a78c78d
Robert A Petit III <robert.petit@emory.edu>
2014-07-21 02:19:00 -0400

    Contact form is working on Peregrine, next test is AWS EC2

--- staphopia/models.py
+++ staphopia/models.py
@@ -1,8 +1,7 @@
 from django import forms
-from django.core.mail import send_mail, BadHeaderError
 
 class ContactForm(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
-    subject = forms.CharField()
-    message = forms.CharField(widget=forms.Textarea())
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField(max_length=100)
+    subject = forms.CharField(max_length=200)
+    message = forms.CharField(widget=forms.Textarea(), max_length=1000)
--- staphopia/views.py
+++ staphopia/views.py
@@ -1,10 +1,7 @@
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
+from django.core.mail import EmailMessage
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from staphopia.models import ContactForm
-from django.template import RequestContext, Context
-from django import forms
-from django.core.mail import send_mail, BadHeaderError
 
 def index(request):
     return render_to_response('index.html', {}, RequestContext(request))
@@ -13,19 +10,26 @@ def login(request):
     return render_to_response('account/login.html')
     
 def contact(request):
-    subject = request.POST.get('subject', '')
-    message = request.POST.get('message', '')
-    from_email = request.POST.get('email', '')
+    if request.POST:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            labrat = "Staphopia's Friendly Robot <usa300@staphopia.com>"
+            sender = '{0} <{1}>'.format(form.cleaned_data['name'], 
+                                        form.cleaned_data['email'])
+            subject = '[Staphopia Contact] - ' + form.cleaned_data['subject']
+            message = ("This is an autmated reply from Staphopia, we'll try to "
+                       "answer you as quickly as possible.\n\n----------\n\n")
+            message += form.cleaned_data['message']
+            recipients = ['admin@staphopia.com', sender]
+            email = EmailMessage(subject, message, labrat, recipients)
+            email.send(fail_silently=False)
 
-    if subject and message and from_email:
-        try:
-            send_mail(subject, message, from_email, ['change@this.com'])
-        except BadHeaderError:
-            return HttpResponse('Invalid header found.')
-        return HttpResponseRedirect('/contact/thankyou/')
+            return render_to_response(
+                'contact/success.html',
+                {},
+                context_instance=RequestContext(request))
     else:
-        return render_to_response('contact.html', {'form': ContactForm()}, 
-                                  RequestContext(request))
+        form = ContactForm()
 
-def thankyou(request):
-    return render_to_response('thankyou.html')
+    return render_to_response('contact.html', {'form': form},
+                              context_instance=RequestContext(request))
