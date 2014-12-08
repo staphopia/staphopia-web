b4cb77f8e2adda5a1391980c618cb81963d33a53
Robert A Petit III <robbie.petit@gmail.com>
2014-12-08 15:28:58 -0500

    setting up for EC2 usage

---
+++ config/gunicorn.conf.py
@@ -0,0 +1,13 @@
+bind = "127.0.0.1:80"
+
+workers = 8
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+errorlog = '/home/staphopia/staphopia.com/logs/error.log'
+loglevel = 'info'
+accesslog = '/home/staphopia/staphopia.com/logs/access.log'
+
+preload = True
--- staphopia/settings/dev.py
+++ staphopia/settings/dev.py
@@ -1,41 +1,39 @@
 from staphopia.settings.common import *
 
-'''-----------------------------------------------------------------------------
-SECURITY WARNING: don't run with debug turned on in production!
------------------------------------------------------------------------------'''
+'''----------------------------------------------------------------------------
+SECURITY WARNING: don't run with debug turned on in production
+----------------------------------------------------------------------------'''
 DEBUG = True
 TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = ['0.0.0.0:80']
 
 
-
-'''-----------------------------------------------------------------------------
+'''----------------------------------------------------------------------------
 Database
 https://docs.djangoproject.com/en/1.6/ref/settings/#databases
------------------------------------------------------------------------------'''
+----------------------------------------------------------------------------'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'staphopia_django',
-        'USER' : 'django',
-        'PASSWORD' : 'ca1e0a1ac201e4',
-        'HOST' : 'localhost',
-        'PORT' : '5432',
+        'NAME': 'staphopia',
+        'USER': 'staphopia',
+        'PASSWORD': 'ca1e0a1ac201e4',
+        'HOST': 'staphopia-dev.cpphjf4vstco.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     },
     'staphopia': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'staphopia',
-        'USER' : 'DeepThought',
-        'PASSWORD' : 'da208f6115574ef429684fa388b04fc958da12c7d2bd2c4d',
-        'HOST' : 'staphopiadev.cpphjf4vstco.us-east-1.rds.amazonaws.com',
-        'PORT' : '29466',
+        'USER': 'DeepThought',
+        'PASSWORD': 'da208f6115574ef429684fa388b04fc958da12c7d2bd2c4d',
+        'HOST': 'staphopiadev.cpphjf4vstco.us-east-1.rds.amazonaws.com',
+        'PORT': '29466',
     }
 }
 
 
-
-'''-----------------------------------------------------------------------------
+'''----------------------------------------------------------------------------
 Static files (CSS, JavaScript, Images)
 https://docs.djangoproject.com/en/1.6/howto/static-files/
------------------------------------------------------------------------------'''
+----------------------------------------------------------------------------'''
 STATIC_URL = '/static/'
