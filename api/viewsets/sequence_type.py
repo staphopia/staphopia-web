089c36f1d3c7a14f00b2b0a094bb6648ee01fae7
Robert A Petit III <robbie.petit@gmail.com>
2015-10-24 13:34:13 +0000

    API Function: Get SCCmec status by ST

--- api/viewsets/sequence_type.py
+++ api/viewsets/sequence_type.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 from django.db import connection
 
 from rest_framework import viewsets
@@ -25,7 +27,8 @@ class MLSTViewSet(viewsets.ReadOnlyModelViewSet):
             result.append({
                 'st': row[0],
                 'count': row[1],
-                'url': '{0}{1}/samples/'.format(base_url, row[0])
+                'get_samples': '{0}{1}/samples/'.format(base_url, row[0]),
+                'get_sccmec_status': '{0}{1}/sccmec/'.format(base_url, row[0])
             })
 
         return Response(result)
@@ -40,3 +43,42 @@ class MLSTViewSet(viewsets.ReadOnlyModelViewSet):
             q, many=True, context={'request': request}
         )
         return Response(serializer.data)
+
+    @detail_route(methods=['get'])
+    def sccmec(self, request, pk=None):
+        """
+        Stored metadata information for a given sample.
+        """
+        sequence_type = pk
+        cutoff = request.GET['cutoff'] if 'cutoff' in request.GET else 0.70
+        only_positive = True if 'only_positive' in request.GET else False
+
+        cursor = connection.cursor()
+        q = "SELECT * FROM sccmec_status_by_st({0})".format(sequence_type)
+        cursor.execute(q)
+
+        results = []
+        exact_matches = 0
+        for row in cursor.fetchall():
+            status = 'positive' if row[2] >= float(cutoff) else 'negative'
+            if only_positive and status is 'negative':
+                continue
+
+            if row[3]:
+                exact_matches += 1
+
+            results.append({
+                'sample_tag': row[0],
+                'st': row[1],
+                'sccmec_status': status,
+                'mecA_coverage': row[2],
+                'st_is_exact_match': row[3]
+            })
+
+        data = OrderedDict((
+            ('total', len(results)),
+            ('exact_st_matches', exact_matches),
+            ('results', results)
+        ))
+
+        return Response(data)
