dab0dfc366839ac6b00379a100c47f93889f3262
Robert A. Petit III <robbie.petit@gmail.com>
2015-11-17 21:57:56 +0000

    added www settings removed old settings

--- staphopia/settings/www.py
+++ staphopia/settings/www.py
@@ -1,4 +1,5 @@
 from staphopia.settings.common import *
+from staphopia.settings.private import DEV_PASS
 
 '''----------------------------------------------------------------------------
 SECURITY WARNING: don't run with debug turned on in production!
@@ -15,19 +16,11 @@ https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'staphopia_django',
-        'USER': 'django',
-        'PASSWORD': 'ca1e0a1ac201e4',
-        'HOST': 'staphopia-django.cpphjf4vstco.us-east-1.rds.amazonaws.com',
-        'PORT': '5432',
-    },
-    'staphopia': {
-        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'staphopia',
-        'USER': 'DeepThought',
-        'PASSWORD': 'da208f6115574ef429684fa388b04fc958da12c7d2bd2c4d',
-        'HOST': 'staphopiadev.cpphjf4vstco.us-east-1.rds.amazonaws.com',
-        'PORT': '29466',
+        'USER': 'staphopia',
+        'PASSWORD': DEV_PASS,
+        'HOST': 'staphopia.cpphjf4vstco.us-east-1.rds.amazonaws.com',
+        'PORT': '30022',
     }
 }
 
@@ -36,8 +29,10 @@ DATABASES = {
 Static files via Amazon S3 (CSS, JavaScript, Images)
 https://docs.djangoproject.com/en/1.6/howto/static-files/
 ----------------------------------------------------------------------------'''
-INSTALLED_APPS += ('storages',)
-AWS_STORAGE_BUCKET_NAME = "staphopia-django"
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = S3_URL
+#INSTALLED_APPS += ('storages',)
+#AWS_STORAGE_BUCKET_NAME = "staphopia-django"
+#STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+#STATIC_URL = S3_URL
+STATIC_URL = '/static/'
+SITE_ID = 1
