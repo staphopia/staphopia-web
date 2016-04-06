e934a88217204968def9ad2ccfaf5ab13e0e3803
Robert A Petit III <robbie.petit@gmail.com>
2016-04-06 11:38:30 -0400

    fixed a few bugs

--- ena/management/commands/update_ena.py
+++ ena/management/commands/update_ena.py
@@ -65,12 +65,12 @@ class Command(BaseCommand):
         ]
 
         self.studies_created = 0
-        self.ena_studies = []
+        self.ena_studies = {}
         self.study_fields = ['study_accession', 'secondary_study_accession',
                              'study_title', 'study_alias']
 
         self.exps_created = 0
-        self.ena_exps = []
+        self.ena_exps = {}
         self.exp_fields = [
             'experiment_accession', 'experiment_title', 'experiment_alias',
             'study_accession', 'sample_accession',
@@ -81,7 +81,7 @@ class Command(BaseCommand):
         ]
 
         self.runs_created = 0
-        self.ena_runs = []
+        self.ena_runs = {}
         self.run_fields = [
             'run_accession', 'experiment_accession', 'is_paired', 'run_alias',
             'read_count', 'base_count', 'mean_read_length', 'coverage',
@@ -138,58 +138,63 @@ class Command(BaseCommand):
                     except:
                         continue
 
-                    self.ena_studies.append({
-                        'study_accession': c['study_accession'],
-                        'secondary_study_accession': (
-                            c['secondary_study_accession']
-                        ),
-                        'study_title': ' '.join(c['study_title'].split()),
-                        'study_alias': c['study_alias'],
-                    })
+                    if c['study_accession'] not in self.ena_studies:
+                        self.ena_studies[c['study_accession']] = {
+                            'study_accession': c['study_accession'],
+                            'secondary_study_accession': (
+                                c['secondary_study_accession']
+                            ),
+                            'study_title': ' '.join(c['study_title'].split()),
+                            'study_alias': c['study_alias'],
+                        }
 
-                    self.ena_exps.append({
-                        'experiment_accession': c['experiment_accession'],
-                        'experiment_title': (
-                            ' '.join(c['experiment_title'].split())
-                        ),
-                        'experiment_alias': c['experiment_alias'],
-                        'study_accession': Study(
-                            study_accession=c['study_accession']
-                        ),
-                        'sample_accession': c['sample_accession'],
-                        'secondary_sample_accession': (
-                            c['secondary_sample_accession']
-                        ),
-                        'submission_accession': c['submission_accession'],
-                        'tax_id': c['tax_id'],
-                        'scientific_name': c['scientific_name'],
-                        'instrument_platform': c['instrument_platform'],
-                        'instrument_model': c['instrument_model'],
-                        'library_layout': c['library_layout'],
-                        'library_strategy': c['library_strategy'],
-                        'library_selection': c['library_selection'],
-                        'center_name': c['center_name'],
-                    })
+                    if c['experiment_accession'] not in self.ena_exps:
+                        self.ena_exps[c['experiment_accession']] = {
+                            'experiment_accession': c['experiment_accession'],
+                            'experiment_title': (
+                                ' '.join(c['experiment_title'].split())
+                            ),
+                            'experiment_alias': c['experiment_alias'],
+                            'study_accession': Study(
+                                study_accession=c['study_accession']
+                            ),
+                            'sample_accession': c['sample_accession'],
+                            'secondary_sample_accession': (
+                                c['secondary_sample_accession']
+                            ),
+                            'submission_accession': c['submission_accession'],
+                            'tax_id': c['tax_id'],
+                            'scientific_name': c['scientific_name'],
+                            'instrument_platform': c['instrument_platform'],
+                            'instrument_model': c['instrument_model'],
+                            'library_layout': c['library_layout'],
+                            'library_strategy': c['library_strategy'],
+                            'library_selection': c['library_selection'],
+                            'center_name': c['center_name'],
+                        }
 
-                    self.ena_runs.append({
-                        'experiment_accession': Experiment(
-                            experiment_accession=c['experiment_accession']
-                        ),
-                        'is_paired': (
-                            '1' if c['library_layout'] == 'PAIRED' else '0'
-                        ),
-                        'run_accession': c['run_accession'],
-                        'run_alias': c['run_alias'],
-                        'read_count': c['read_count'],
-                        'base_count': c['base_count'],
-                        'mean_read_length': '{0:.2f}'.format(mean_read_length),
-                        'coverage': '{0:.2f}'.format(coverage),
-                        'first_public': c['first_public'],
-                        'fastq_bytes': c['fastq_bytes'],
-                        'fastq_md5': c['fastq_md5'],
-                        'fastq_aspera': c['fastq_aspera'],
-                        'fastq_ftp': c['fastq_ftp'],
-                    })
+                    if c['run_accession'] not in self.ena_runs:
+                        self.ena_runs[c['run_accession']] = {
+                            'experiment_accession': Experiment(
+                                experiment_accession=c['experiment_accession']
+                            ),
+                            'is_paired': (
+                                '1' if c['library_layout'] == 'PAIRED' else '0'
+                            ),
+                            'run_accession': c['run_accession'],
+                            'run_alias': c['run_alias'],
+                            'read_count': c['read_count'],
+                            'base_count': c['base_count'],
+                            'mean_read_length': '{0:.2f}'.format(
+                                mean_read_length
+                            ),
+                            'coverage': '{0:.2f}'.format(coverage),
+                            'first_public': c['first_public'],
+                            'fastq_bytes': c['fastq_bytes'],
+                            'fastq_md5': c['fastq_md5'],
+                            'fastq_aspera': c['fastq_aspera'],
+                            'fastq_ftp': c['fastq_ftp'],
+                        }
                 else:
                     self.missing += 1
 
@@ -197,7 +202,7 @@ class Command(BaseCommand):
     def insert(self, ena_data, ena_obj):
         """Insert experiment results to the database."""
         total_created = 0
-        for row in ena_data:
+        for key, row in ena_data.items():
             try:
                 obj, created = ena_obj.objects.update_or_create(
                     defaults=row, **row
