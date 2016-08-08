80130f88090199dd4db5cc9e47a1899807614e53
Robert A Petit III <robbie.petit@gmail.com>
2016-08-08 16:00:31 -0400

    revert local settings

--- staphopia/settings/local.py
+++ staphopia/settings/local.py
@@ -18,7 +18,7 @@ DATABASES = {
         'NAME': 'staphopia',
         'USER': 'staphopia',
         'PASSWORD': DEV_PASS,
-        'HOST': 'staphopia.emory.edu',
+        'HOST': 'staphopia.genetics.emory.edu',
         'PORT': '29466',
     }
 }
