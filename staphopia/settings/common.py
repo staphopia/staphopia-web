ca6543300d8478950c1597caa114b1c50deccf89
Robert A Petit III <robbie.petit@gmail.com>
2017-02-06 14:32:07 -0500

    changes to config

--- staphopia/settings/common.py
+++ staphopia/settings/common.py
@@ -87,7 +87,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_THROTTLE_RATES': {
         'anon': '10/day',
-        'user': '10000/day'
+        'user': '1000000/day'
     },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
