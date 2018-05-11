349ac86d7545420df0d92c2ca0f19c889c589e37
Robert A Petit III <robbie.petit@gmail.com>
2018-05-11 12:04:24 -0400

    fixed auto merge issue

--- api/views.py
+++ api/views.py
@@ -4,8 +4,5 @@ from django.shortcuts import render, redirect
 def api_token(request):
     if request.user.is_authenticated:
         return render(request, 'settings/api-token.html')
-<<<<<<< HEAD
-    return redirect(request, 'accounts/login/')
-=======
     return redirect('/accounts/login/')
->>>>>>> 9f859aeb4fa94c876d736e9596574c0d656c4d95
+
